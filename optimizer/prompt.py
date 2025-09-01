3c6c177fd0db64860d232d32e5b68b2eb1a99ac0
KamranM <2909367+KamranMK@users.noreply.github.com>
2025-09-01 15:17:26 +0200

    Update prompt.py

--- optimizer/prompt.py
+++ optimizer/prompt.py
@@ -116,3 +116,60 @@ if __name__ == "__main__":
         
     except Exception as e:
         logger.error(f"Debug execution failed: {e}", exc_info=True)
+
+
+# This is my class for anthropic bedrock calls
+import logging
+import instructor
+import boto3
+from typing import List, Type
+from pydantic import BaseModel
+
+class AnthropicBedrockChatCompletions:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def create(
+        self,
+        modelId: str,
+        max_tokens: int,
+        system_message: str | List,
+        messages: List,
+        response_model: BaseModel | Type[BaseModel],
+        dump: bool = True
+    ):
+        """
+        Send prompt and acquire the chat completions response.
+
+        :param modelId str: the llm model we are going to use
+        :param max tokens: token limitation
+        :param system_message: system_message for api call
+        :param messages: list of client messages
+        :param target_key: key of target field
+        :param response_model: tools we are using
+        """
+        bedrock_client = boto3.client('bedrock-runtime')
+        client = instructor.from_bedrock(bedrock_client)
+
+        # convert system message to list in case its not
+        if not isinstance(system_message, List):
+            system_message = [{'role': 'system', 'content': system_message}]
+
+        # try
+        try:
+            resp = client.chat.completions.create(
+                modelId=modelId,
+                max_tokens=max_tokens,
+                system=system_message,
+                messages=messages,
+                response_model=response_model
+            )
+            self.logger.info("Prompt is successful")
+            # return the pydantic object without introducing breaking change
+            if not dump:
+                return resp
+            return resp.model_dump()
+
+        except Exception as e:
+            self.logger.error(f"Could not complete prompt due to: {str(e)}")
+            raise RuntimeError(f"Could not complete prompt due to: {str(e)}")
