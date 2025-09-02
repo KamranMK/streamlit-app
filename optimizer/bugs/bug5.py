8f2dfe8cc8f0eae1371ed8a255a20b6e79f794a4
KamranM <2909367+KamranMK@users.noreply.github.com>
2025-09-02 10:52:48 +0200

    Create bug5.py

---
+++ optimizer/bugs/bug5.py
@@ -0,0 +1,12 @@
+INFO:OfflineTrainingSetup:
+🎉 Offline setup is working perfectly!
+INFO:OfflineTrainingSetup:🚀 Ready for full optimization. To train:
+INFO:adalflow.optim.few_shot.bootstrap_optimizer:BootstrapFewShot: ['few_shot_demos']
+INFO:adalflow.core.prompt_builder:Prompt has variables: ['schema']
+INFO:adalflow.core.prompt_builder:Prompt has variables: ['schema']
+INFO:adalflow.core.prompt_builder:Prompt has variables: ['instruction_to_optimizer', 'output_format_str']
+INFO:adalflow.core.prompt_builder:Prompt has variables: ['variable', 'peers']
+INFO:adalflow.optim.grad_component:EvalFnToTextLoss: No backward engine provided. Creating one using model_client and model_kwargs.
+INFO:adalflow.core.generator:Generator Generator initialized.
+raw_shots: 0, bootstrap_shots: 1
+BootstrapFewShot: ['few_shot_demos']
